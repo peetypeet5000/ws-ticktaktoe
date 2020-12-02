d6f654b4595414ffd01f054f7713bd6fa0e9ed5b
Peter LaMontagne <plamon42@gmail.com>
2020-12-02 14:25:53 -0800

    basic templatizing, added public folder, basic server setup, gitignore

---
+++ server.js
@@ -0,0 +1,46 @@
+/*
+ * Write your routing code in this file.  Make sure to add your name and
+ * @oregonstate.edu email address below.
+ *
+ * Name: Peter LaMontagne
+ * Email: lamontap@oregonstate.edu
+ */
+
+
+//import modules and ata
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+//setup express
+var app = express();
+var port = process.env.PORT || 3000;
+
+//setup templating engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+app.use(express.static('public'));
+
+
+
+
+//serve the main page
+app.get('/', function (req, res) {
+  res.status(200).render('mainPage');
+});
+
+
+//404 page
+app.get('*', function (req, res) {
+  res.status(404).render('404', {
+    url: req.url
+  });
+});
+
+
+
+
+app.listen(port, function () {
+  console.log("== Server is listening on port", port);
+});
