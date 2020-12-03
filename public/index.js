ec01bb6805fb69a2da8d201f8fea7b1790de7a1e
Peter LaMontagne <plamon42@gmail.com>
2020-12-03 00:23:24 -0800

    added basic visual game updates, added basic server async comm.

---
+++ public/index.js
@@ -0,0 +1,89 @@
+//called when any button clicked
+function buttonClicked(event) {
+    //will get the value of the button, 0-8
+    var buttonSelected = event.target.dataset.value;
+
+    //if the square is not empty
+    if(gameState.board[buttonSelected] != 0) {
+        alert("Invalid Move!");
+    } else {
+        //set to players int, so 1 for player 1 and 2 for player 2
+        gameState.board[buttonSelected] = gameState.activePlayer;
+    }
+
+    //advance to next turn
+    if(gameState.activePlayer == 2) {
+        gameState.activePlayer = 1;
+    } else {
+        gameState.activePlayer = 2;
+    }
+
+    console.log("== Move recorded. Current gameState: ", gameState);
+
+    updateBoard();
+
+    sendRequest();
+}
+
+
+//visually updates the board based on gameState
+function updateBoard() {
+
+    //grabs all the div's from the gameboard container
+    var allBoxes = ticTac_board.querySelectorAll("div");
+
+    //loop thru every box, update its visual style if it was played
+    //its 0-18 because that query selection returns the box & image divs
+    for(var i = 0; i < 18; i = i + 2) {
+        var currentBox = allBoxes[i];
+   
+        //if 1, tick, if 2, tack
+        if(gameState.board[i / 2] == 1){
+            currentBox.classList.add("box-tick");
+        } else if(gameState.board[i / 2] == 2) {
+            currentBox.classList.add("box-tack");
+        }
+    }
+}
+
+
+
+//sends new gameState to server
+function sendRequest() {
+    var request = new XMLHttpRequest();
+    var gameStateString = JSON.stringify(gameState);
+
+    //specifies POST method on /move
+    request.open('POST', '/move');
+
+    //let server know its a JSON request
+    request.setRequestHeader(
+        'Content-Type', 'application/json'
+    );
+
+    //will get called once the responce is sent back
+    request.addEventListener('load', function (event) {
+        console.log(event.target.status);
+        console.log("== Server Responce: ", event.target.response);
+    });
+
+    //actually send request
+    request.send(gameStateString);
+
+}
+
+
+
+
+//JSON object to hold active gamestate
+var gameState = {
+    activePlayer: 1,
+    board: [
+        0, 0, 0, 0, 0, 0, 0, 0, 0
+    ]
+}
+
+/******* Event Listeners **********/
+var buttonsContainer = document.getElementById("ticTac_board");
+buttonsContainer.addEventListener('click', buttonClicked, false);
+
--- server.js
+++ server.js
@@ -6,28 +6,50 @@ var exphbs = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
 
+//setup active gamestate
+var gameStates = {
+  activePlayer: 1,
+  board: [
+      0, 0, 0, 0, 0, 0, 0, 0, 0
+  ]
+}
+
 //setup templating engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+//stores received json in req.body
+app.use(express.json());
 
+//serves static files in /public
 app.use(express.static('public'));
 
 
 //serve the main page
 app.get('/', function (req, res) {
-  res.status(200).render('mainPage');
+  res.status(200).render('mainPage', {
+    title: "Three n' a row!"
+  });
 });
 
 
 //404 page
 app.get('*', function (req, res) {
   res.status(404).render('404', {
-    url: req.url
+    url: req.url,
+    title: '404 - Page not found'
   });
 });
 
 
+
+app.post('/move', function(req, res, next) {
+  console.log("== Current Gamestate: ", req.body, "\n");
+
+  if(req.body && req.body.activePlayer && req.body.board) {
+    res.status(200).send("Move recorded");
+  }
+})
 
 
 app.listen(port, function () {
