2fd927c986d95577fcc63294a71d39801fce5ce8
Peter LaMontagne <plamon42@gmail.com>
2020-12-07 20:41:47 -0800

    fixed merge issue -- code should now all be present

--- server.js
+++ server.js
@@ -15,10 +15,11 @@ var gameState = {
 	type: "gameState",
 	activePiece: 1, //1 or 2 -- determines tick or tack
 	currentPlayer: 1, //starts at 1 -- used for player queue
+	totalPlayers: 0,
 	board: [
-			0, 0, 0, 0, 0, 0, 0, 0, 0
+		0, 0, 0, 0, 0, 0, 0, 0, 0
 	]
-}
+  }
 
 
 //will hold the soocket objects for connected websocket clients
@@ -57,88 +58,58 @@ app.get('*', function (req, res) {
 //handle websocket request
 webSocketServer.on('connection', socket => {
 
-	//when a new user connects, send them gamestate
-	socket.send(JSON.stringify(gameState));
-
 	//update list of players, assign new player their #
-	updatePlayerList(socket);
-
-
-	//this is where websocket requests are actually handled
-	socket.on('message', message => {
-
-		//print message, convert to JSON object
-		console.log("== Client Message Recived: ", message)
-		var objRecevied = JSON.parse(message)
-
-		//checks the type key in JSON object
-		switch(objRecevied.type) {
-			case "gameState":
-				//if we get a new gamestate, overwrite and sync clients
-				gameState = objRecevied;
-				updateGameState();
-				updateClients();
-				break;
-		}
-
-	})
-});
-
-
-function checkWin(gameState)
-{
-
-// Is there a more elegant way to do this?  Definitely
-// Do I know how to do it?  Yup!
-// Do I have the mental energy to implement the more elegant solution at this point in the term?
-// Nope.
-if ((gameState.board[0] == gameState.board [1]) && (gameState.board[1] == gameState.board[2]) && (gameState.board[1] != 0)) //check row 1 MUDA
-  return true; // ORA
-else if ((gameState.board[3] == gameState.board[4]) && (gameState.board[4] == gameState.board[5]) && (gameState.board[4] != 0)) // check row 2 MUDA
-  return true; // ORA
-else if ((gameState.board[6] == gameState.board[7]) && (gameState.board[7] == gameState.board[8]) && (gameState.board[7] != 0)) // check row 3 MUDA
-  return true; // ORA
-else if ((gameState.board[0] == gameState.board[3]) && (gameState.board[3] == gameState.board[6]) && (gameState.board[3] != 0)) // check column 1 MUDA
-  return true; // ORA
-else if ((gameState.board[1] == gameState.board[4]) && (gameState.board[4] == gameState.board[7]) && (gameState.board[4] != 0)) // Check column 2 MUDA
-  return true; // ORA
-else if ((gameState.board[2] == gameState.board[5]) && (gameState.board[5] == gameState.board[8]) && (gameState.board[5] != 0)) // Check column 3 MUDA
-  return true; // ORA
-else if ((gameState.board[0] == gameState.board[4]) && (gameState.board[4] == gameState.board[8]) && (gameState.board[4] != 0)) // Check diagonal 1 MUDA
-  return true; // ORA
-else if ((gameState.board[2] == gameState.board[4]) && (gameState.board[4] == gameState.board[6]) && (gameState.board[4] != 0)) // Check diagonal 2 MUDA
-  return true; // ORA
-else {
-  return false; // WRRRRRRRRRRRRRRRRRRRRRRRRRRRRY
-}
-}
-
-function checkTie(gameState)
-{
-  for (i = 0; i <= 9; i++) // Cycles through the board array
-  {
-    if (gameState.board[i] == 0) // If we find a blank spot
-    return false; // There's no tie
-  }
-  return true; // We've cycled through the board, there are no empty spaces, game is a tie.
-}
-
-
-//adds all new clients to array of clients, sends client their spot in array
-function updatePlayerList(socket) {
 	currentPlayers.push(socket); //add to array
 	console.log("== New Player Joined. Current Players: ", currentPlayers.length);
+  
+	//when a new user connects, send them gamestate
+	//socket.send(JSON.stringify(gameState));
+	updatePlayerNumbers();
+	updateClients();
+  
+  
+  
+	//this is where websocket requests are actually handled
+	socket.on('message', message => {
+  
+	  //print message, convert to JSON object
+	  console.log("== Client Message Recived: ", message)
+	  var objRecevied = JSON.parse(message)
+  
+	  //checks the type key in JSON object
+	  switch(objRecevied.type) {
+		case "gameState":
+		  //if we get a new gamestate, overwrite and sync clients
+		  gameState = objRecevied;
+		  updateGameState();
+		  updateClients();
+		  break;
+	  }
+  
+	})
+  
+	socket.on('close', message => { 
+	  //updates array of players
+  
+	  console.log("= Player Disconnecting: ", currentPlayers.length);
+	  //filters our dead socket
+	  var newCurrentPlayers = currentPlayers.filter(function (ele) {
+		if(ele.readyState == 1) {
+		  return ele;
+		}
+	  })
+  
+	  currentPlayers = newCurrentPlayers;
+  
+	  console.log("= Player Disconnected. There is now ", currentPlayers.length, " players.");
+  
+	  //update clients
+	  updatePlayerNumbers();
+	
+	})
+  });
 
-	//the JSON object to send to new client
-	var message = JSON.stringify({
-		type: "assignPlayer",
-		playerInt: currentPlayers.length,
-	});
 
-	//send to client
-	socket.send(message);
-
-}
 
 
 
@@ -161,18 +132,24 @@ function updateClients() {
  ******************************/
 function updateGameState() {
 
-	//advance gamestate to next player in queue
-	gameState.currentPlayer++;
-	if(gameState.currentPlayer > currentPlayers.length) {
-		gameState.currentPlayer = 1;
-	}
+  //advance gamestate to next player in queue
+  gameState.currentPlayer++;
+  if(gameState.currentPlayer >= (currentPlayers.length + 1)) {
+    gameState.currentPlayer = 1;
+  }
 
-	//advance to next piece (tick or tack)
-	if(gameState.activePiece == 2) {
-		gameState.activePiece = 1;
-	} else {
-		gameState.activePiece = 2;
-	}
+  //check if this player is still connected
+  //checkValidPlayer();
+
+  //advance to next piece (tick or tack)
+  if(gameState.activePiece == 2) {
+	gameState.activePiece = 1;
+  } else {
+	gameState.activePiece = 2;
+  }
+
+  //update total number of players
+  gameState.totalPlayers = currentPlayers.length;
 
 	//if win or tie, reset board
 	if(checkWin() == true || checkTie() == true) {
@@ -182,6 +159,31 @@ function updateGameState() {
 		]
 	}
 }
+
+
+
+//loops thru all connected players and assigns them their number in the queue
+function updatePlayerNumbers() {
+	var playerNumber = 1;
+  
+	webSocketServer.clients.forEach(function each(client) {
+	  //only open clients
+	  if (client.readyState === ws.OPEN) {
+  
+		  //the JSON object to send to new client
+		  var newPlayer = {
+			type: "assignPlayer",
+			playerInt: playerNumber,
+		  };
+  
+  
+		  //send JSON of gameState object
+		  client.send(JSON.stringify(newPlayer));
+  
+		  playerNumber++;
+	  }
+	});
+  }
 
 
 
